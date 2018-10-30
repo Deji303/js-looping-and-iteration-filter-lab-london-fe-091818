01602e7b9f93b80276e3566e5f73b3f9c30e1737
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 19:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function fuzzyMatch(drivers, string) {
   return drivers.filter(d => d[0] === string[0] )
 }
 
-function 
+function matchName (drivers, string) {
+  return drivers.filter(d => d.name === string)
+}
